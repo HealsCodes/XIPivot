2fd622a648686c4d21e4f6af94c92bbe2b60786f
Renee Koecher <info@bitspin.org>
2019-11-01 18:33:13 +0000

    Ashita: bump to 0.4.0

--- XIPivot.Ashita/plugin/plugin.cpp
+++ XIPivot.Ashita/plugin/plugin.cpp
@@ -42,7 +42,7 @@ extern "C" {
 		strcpy_s(info->Name, sizeof(info->Name), "XIPivot");
 
 		info->InterfaceVersion = ASHITA_INTERFACE_VERSION;
-		info->PluginVersion = 0.31f;
+		info->PluginVersion = 0.40f;
 		info->Priority = 0;
 	}
 
