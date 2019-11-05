4fe273c291946f71a421de671e0757e33ad246f2
Renee Koecher <info@bitspin.org>
2019-11-05 16:37:54 +0100

    Basic MemCache implementation to store / expire file data for faster I/O

---
+++ XIPivot.Core/src/MemCache.cpp
@@ -0,0 +1,377 @@
+/*
+ * 	Copyright © 2019, Renee Koecher
+ * 	All rights reserved.
+ * 
+ * 	Redistribution and use in source and binary forms, with or without
+ * 	modification, are permitted provided that the following conditions are met :
+ * 
+ * 	* Redistributions of source code must retain the above copyright
+ * 	  notice, this list of conditions and the following disclaimer.
+ * 	* Redistributions in binary form must reproduce the above copyright
+ * 	  notice, this list of conditions and the following disclaimer in the
+ * 	  documentation and/or other materials provided with the distribution.
+ * 	* Neither the name of XIPivot nor the
+ * 	  names of its contributors may be used to endorse or promote products
+ * 	  derived from this software without specific prior written permission.
+ * 
+ * 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * 	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * 	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * 	DISCLAIMED.IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * 	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * 	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * 	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * 	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * 	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * 	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "MemCache.h"
+#include "detours.h"
+
+#include <cctype>
+#include <ctime>
+
+namespace XiPivot
+{
+	namespace Core
+	{
+		/* static member initialisation */
+
+		MemCache* MemCache::s_instance = nullptr;
+
+		MemCache::pFnReadFile    MemCache::s_procReadFile = ReadFile;
+		MemCache::pFnCloseHandle MemCache::s_procCloseHandle = CloseHandle;
+
+		/* static interface */
+		MemCache& MemCache::instance(void)
+		{
+			if (MemCache::s_instance == nullptr)
+			{
+				MemCache::s_instance = new MemCache();
+			}
+			return *MemCache::s_instance;
+		}
+
+		BOOL __stdcall MemCache::dReadFile(HANDLE a0, LPVOID a1, DWORD a2, LPDWORD a3, LPOVERLAPPED a4)
+		{
+			/* don't use the Singleton access here;
+			 * if for whatever reason the global object is gone we don't want a new one
+			 */
+			if (MemCache::s_instance != nullptr || a4 != nullptr)
+			{
+				return MemCache::s_instance->interceptReadFile(a0, a1, a2, a3, a4);
+			}
+			return MemCache::s_procReadFile(a0, a1, a2, a3, a4);
+		}
+
+		BOOL __stdcall MemCache::dCloseHandle(HANDLE a0)
+		{
+			/* don't use the Singleton access here;
+			 * if for whatever reason the global object is gone we don't want a new one
+			 */
+			if (MemCache::s_instance != nullptr)
+			{
+				return MemCache::s_instance->interceptCloseHandle(a0);
+			}
+			return MemCache::s_procCloseHandle(a0);
+		}
+
+		MemCache::MemCache()
+			: m_hooksSet(false),
+			  m_stats({ 0, 0, 0, 0, 0 }),
+			  m_logDebug(ILogProvider::LogLevel::Discard)
+		{
+			m_logger = DummyLogProvider::instance();
+		}
+
+		MemCache::~MemCache()
+		{
+			releaseHooks(); // just in case
+
+			m_cachePointers.clear();
+
+			purgeCacheObjects(0);
+			m_cacheObjects.clear();
+		}
+
+		void MemCache::setLogProvider(ILogProvider* newLogProvider)
+		{
+			if (newLogProvider == nullptr)
+			{
+				return;
+			}
+			m_logger = newLogProvider;
+		}
+
+		bool MemCache::setupHooks(void)
+		{
+			if (m_hooksSet == false)
+			{
+				/* hooks need to be set */
+				m_hooksSet = true;
+				DetourTransactionBegin();
+				DetourUpdateThread(GetCurrentThread());
+
+				DetourAttach(&(PVOID&)MemCache::s_procReadFile, MemCache::dReadFile);
+				DetourAttach(&(PVOID&)MemCache::s_procCloseHandle, MemCache::dCloseHandle);
+
+				m_hooksSet = DetourTransactionCommit() == NO_ERROR;
+
+				m_logger->logMessageF(ILogProvider::LogLevel::Info, "m_hooksSet = %s", m_hooksSet ? "true" : "false");
+				return m_hooksSet;
+			}
+			m_logger->logMessage(m_logDebug, "hooks already set");
+			return false;
+		}
+
+		bool MemCache::releaseHooks(void)
+		{
+			if (m_hooksSet == true)
+			{
+				m_hooksSet = false;
+				DetourTransactionBegin();
+				DetourUpdateThread(GetCurrentThread());
+
+				DetourDetach(&(PVOID&)MemCache::s_procReadFile, MemCache::dReadFile);
+				DetourDetach(&(PVOID&)MemCache::s_procCloseHandle, MemCache::dCloseHandle);
+
+				m_hooksSet = (DetourTransactionCommit() == NO_ERROR) ? false : true;
+
+				m_logger->logMessageF(ILogProvider::LogLevel::Info, "m_hooksSet = %s", m_hooksSet ? "true" : "false");
+				return m_hooksSet;
+			}
+			m_logger->logMessage(m_logDebug, "hooks already removed");
+			return false;
+		}
+
+		void MemCache::setDebugLog(bool state)
+		{
+			m_logDebug = (state) ? ILogProvider::LogLevel::Debug : ILogProvider::LogLevel::Discard;
+			m_logger->logMessageF(ILogProvider::LogLevel::Info, "m_logDebug = %s", state ? "Debug" : "Discard");
+		}
+
+		void MemCache::setCacheAllocation(size_t allocationSize)
+		{
+			/* this changes the allowed allocation but it does not trigger a cache purge */
+			m_logger->logMessageF(ILogProvider::LogLevel::Info, "changing cache allocation to %d.%dMB", allocationSize / 0x100000, allocationSize % 0x100000);
+			m_stats.allocation = allocationSize;
+		}
+	
+		/* track and cache a file handle for a given key */
+		HANDLE MemCache::trackCacheObject(HANDLE hRef, int32_t pathKey)
+		{
+			if (m_stats.allocation != 0 && hRef != nullptr && pathKey != -1)
+			{
+				if (m_cachePointers.find(hRef) == m_cachePointers.end()
+					&& getCachedObject(hRef, pathKey) != nullptr)
+				{
+					m_cachePointers.emplace(hRef, CachePointer({ 0, pathKey }));
+					m_logger->logMessageF(ILogProvider::LogLevel::Debug, "started to track HANDLE %p => %d", hRef, pathKey);
+				}
+			}
+			return hRef;
+		}
+
+		size_t MemCache::purgeCacheObjects(time_t maxAge)
+		{
+			size_t objectsPurged = 0;
+			time_t oldAge = time(nullptr) - maxAge;
+
+			for (auto it = m_cacheObjects.cbegin(); it != m_cacheObjects.cend();)
+			{
+				if (it->second->lastUse < oldAge)
+				{
+					m_logger->logMessageF(m_logDebug, "purgeCacheObjects: removing %d (%zd bytes)", it->first, it->second->size);
+
+					auto obj = it->second;
+					m_cacheObjects.erase(it++);
+
+					m_stats.used -= obj->size;
+					--m_stats.activeObjects;
+
+					delete[] obj->data;
+					delete obj;
+
+					++objectsPurged;
+				}
+				else
+				{
+					++it;
+				}
+			}
+			return objectsPurged;
+		}
+
+		/* static hooks */
+
+		BOOL __stdcall
+			MemCache::interceptReadFile(HANDLE a0, LPVOID a1, DWORD a2, LPDWORD a3, LPOVERLAPPED a4)
+		{
+			if (performCachedRead(a0, a1, a2, a3) == true)
+			{
+				return true;
+			}
+			return MemCache::s_procReadFile(a0, a1, a2, a3, a4);
+		}
+
+		BOOL __stdcall
+			MemCache::interceptCloseHandle(HANDLE a0)
+		{
+			const auto it = m_cachePointers.find(a0);
+			if (it != m_cachePointers.end())
+			{
+				m_logger->logMessageF(m_logDebug, "stopped tracking HANDLE %p", a0);
+				m_cachePointers.erase(it);
+			}
+			return MemCache::s_procCloseHandle(a0);
+		}
+
+		/* private stuff */
+
+		MemCache::CacheObject* MemCache::getCachedObject(HANDLE hRef, int32_t pathKey)
+		{
+			auto cachedObj = m_cacheObjects.find(pathKey);
+			if (cachedObj != m_cacheObjects.end())
+			{
+				++m_stats.cacheHits;
+				return cachedObj->second;
+			}
+
+			if (hRef == nullptr)
+			{
+				/* not in cache and no way to create it */
+				++m_stats.cacheMisses;
+				return nullptr;
+			}
+
+			/* try and create a new object on the fly and store it */
+			CacheObject* obj = new (std::nothrow) CacheObject;
+			if (obj != nullptr)
+			{
+				size_t size = GetFileSize(hRef, nullptr);
+
+				if (m_stats.used + size <= m_stats.allocation)
+				{
+					memset(obj, 0, sizeof(CacheObject));
+					obj->data = new (std::nothrow) BYTE[size];
+					obj->size = size;
+
+					if (obj->data != nullptr)
+					{
+						/* read the actual data into memory */
+
+						if(readObjectData(hRef, *obj))
+						{
+							m_stats.used += obj->size;
+							++m_stats.activeObjects;
+
+							obj->lastUse = time(nullptr);
+							m_cacheObjects.emplace(pathKey, obj);
+
+							m_logger->logMessageF(m_logDebug, "getCachedObject: created cache object for %p => %zd bytes", hRef, obj->size);
+
+							++m_stats.cacheMisses;
+							return obj;
+						}
+
+						delete[] obj->data;
+					}
+				}
+				else
+				{
+					m_logger->logMessageF(ILogProvider::LogLevel::Warn, "getCachedObject: chace limit exceeded");
+				}
+				delete obj;
+			}
+			++m_stats.cacheMisses;
+			return nullptr;
+		}
+
+		bool MemCache::readObjectData(HANDLE hRef, CacheObject& obj)
+		{
+			if (hRef == nullptr || obj.data == nullptr)
+			{
+				return false;
+			}
+
+			SetFilePointer(hRef, 0, nullptr, FILE_BEGIN);
+
+			size_t readSize = 0;
+			while (readSize < obj.size)
+			{
+				DWORD bytesRead = 0;
+				if (MemCache::s_procReadFile(hRef, &obj.data[readSize], obj.size - readSize, &bytesRead, nullptr) == FALSE)
+				{
+					m_logger->logMessageF(ILogProvider::LogLevel::Warn, "readObjectData: aborting read with %zd / %zd bytes", readSize, obj.size);
+					break;
+				}
+
+				readSize += bytesRead;
+
+				if (bytesRead == 0)
+				{
+					break;
+				}
+			}
+
+			SetFilePointer(hRef, 0, nullptr, FILE_BEGIN);
+			return readSize == obj.size;
+		}
+
+		bool MemCache::performCachedRead(HANDLE hRef, LPVOID lpBuffer, DWORD bytesToRead, LPDWORD bytesRead)
+		{
+			if (hRef == nullptr || lpBuffer == nullptr)
+			{
+				return false;
+			}
+
+			const auto pointer = m_cachePointers.find(hRef);
+			if (pointer == m_cachePointers.end())
+			{
+				/* not tracked */
+				return false;
+			}
+
+			const auto cacheObj = getCachedObject(nullptr, pointer->second.pathKey);
+			if (cacheObj == nullptr)
+			{
+				/* tracked but no longer cached */
+				m_logger->logMessageF(ILogProvider::LogLevel::Warn, "performCachedRead: cache object for %p (%d) vanished, untracking HANDLE.", hRef, pointer->second.pathKey);
+				m_cachePointers.erase(pointer);
+
+				return false;
+			}
+
+			/* everything valid - touch the object to keep it alive */
+			cacheObj->lastUse = time(nullptr);
+
+			if (pointer->second.offset >= cacheObj->size)
+			{
+				/* already at end of file */
+				bytesToRead = 0;
+			}
+			else if (pointer->second.offset + bytesToRead > cacheObj->size)
+			{
+				/* read to end of file */
+				bytesToRead = cacheObj->size - pointer->second.offset;
+			}
+
+			if (bytesToRead > 0)
+			{
+				memcpy(lpBuffer, &cacheObj->data[pointer->second.offset], bytesToRead);
+				pointer->second.offset += bytesToRead;
+			}
+
+			if (bytesRead != nullptr)
+			{
+				*bytesRead = bytesToRead;
+			}
+
+			/* update the real file pointer in case it is used with other API methods */
+			SetFilePointer(hRef, pointer->second.offset, nullptr, FILE_BEGIN);
+
+			return true;
+		}
+	}
+}
---
+++ XIPivot.Core/src/MemCache.h
@@ -0,0 +1,172 @@
+/*
+ * 	Copyright © 2019, Renee Koecher
+ * 	All rights reserved.
+ * 
+ * 	Redistribution and use in source and binary forms, with or without
+ * 	modification, are permitted provided that the following conditions are met :
+ * 
+ * 	* Redistributions of source code must retain the above copyright
+ * 	  notice, this list of conditions and the following disclaimer.
+ * 	* Redistributions in binary form must reproduce the above copyright
+ * 	  notice, this list of conditions and the following disclaimer in the
+ * 	  documentation and/or other materials provided with the distribution.
+ * 	* Neither the name of XIPivot nor the
+ * 	  names of its contributors may be used to endorse or promote products
+ * 	  derived from this software without specific prior written permission.
+ * 
+ * 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * 	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * 	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * 	DISCLAIMED.IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * 	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * 	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * 	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * 	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * 	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * 	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "LogProvider.h"
+
+#include <Windows.h>
+
+#include <map>
+#include <vector>
+#include <string>
+
+namespace XiPivot
+{
+	namespace Core
+	{
+		/* in-memory file cache to hopefully speed up XI's loading times
+		 *
+		 * the current version is based on the observed read behavior of POL:
+		 * - a file is opened for reading
+		 * - the files contents are sequencially read into memory in chunks
+		 * - the file is closed
+		 *
+		 * POL doesn't seem to seek inside the file so this case is not currently handled.
+		 */
+		class MemCache
+		{
+			/* signature types for the Win32 API methods we're going to hook */
+
+			typedef BOOL(WINAPI * pFnReadFile)(
+				HANDLE       hRef,
+				LPVOID       lpBuffer,
+				DWORD        nNumberOfBytesToRead,
+				LPDWORD      lpNumberOfBytesRead,
+				LPOVERLAPPED lpOverlapped
+				);
+
+			typedef BOOL(WINAPI* pFnCloseHandle)(
+				HANDLE       hRef
+				);
+
+			/* internal statistics tracking */
+			struct CacheStatus
+			{
+				size_t   used;
+				size_t   allocation;
+
+				unsigned cacheHits;
+				unsigned cacheMisses;
+
+				unsigned activeObjects;
+			};
+
+			/* representation of a single cached file */
+			struct CacheObject
+			{
+				size_t   size;
+				PBYTE    data;
+
+				time_t   lastUse;
+			};
+
+			/* pointer from a HANDLE to a cache object */
+			struct CachePointer
+			{
+				DWORD    offset;
+				int32_t  pathKey;
+			};
+
+		public:
+			virtual ~MemCache(void);
+
+			/* setup and tear-down of syscall hooks*/
+			bool setupHooks(void);
+			bool releaseHooks(void);
+
+			bool hooksActive(void) const { return m_hooksSet; };
+
+			/* change the active log provider */
+			void setLogProvider(ILogProvider *logProvider);
+
+			/* toggle debug logging on/off */
+			void setDebugLog(bool state);
+
+			/* change the maximum allowed cache size (in byte) */
+			void setCacheAllocation(size_t allocationSize);
+		
+			/* track and cache a file handle for a given key */
+			HANDLE trackCacheObject(HANDLE hRef, int32_t pathKey);
+
+			/** trigger a purge of cache objects of a certain age 
+			 * @param maxAge maximum time since last access (in seconds)
+			 */
+			size_t purgeCacheObjects(time_t maxAge);
+
+			/* return a copy of the cache usage statistics */
+			CacheStatus getCacheStats(void) const { return m_stats; };
+
+		public:
+			/* access or create the actual Redirector instance */
+			static MemCache& instance(void);
+
+			/* static callbacks used by the Detours library */
+			static BOOL __stdcall dReadFile(HANDLE a0, LPVOID a1, DWORD a2, LPDWORD a3, LPOVERLAPPED a4);
+			static BOOL __stdcall dCloseHandle(HANDLE a0);
+
+		private /* static */:
+
+			static pFnReadFile s_procReadFile;
+			static pFnCloseHandle s_procCloseHandle;
+
+		protected:
+			/* globally unique instance pointer */
+			static MemCache* s_instance;
+
+			/* it would be foolish to allow more than one Redirector to be created,
+			   as such this whole class is designed as a Singleton.
+			 */
+			explicit MemCache(void);
+
+		private:
+			/* actual code to handle the intercept / redirect of file names */
+			BOOL __stdcall interceptReadFile(HANDLE a0, LPVOID a1, DWORD a2, LPDWORD a3, LPOVERLAPPED a4);
+			BOOL __stdcall interceptCloseHandle(HANDLE a0);
+
+			/** create or fetch a cached version of a file handle 
+			 * @param hRef - if not nullptr will be used to create a new object if it doesn't exist
+			 */
+			CacheObject* getCachedObject(HANDLE hRef, int32_t pathKey);
+			bool readObjectData(HANDLE hRef, CacheObject& obj);
+
+			bool performCachedRead(HANDLE hRef, LPVOID lpBuffer, DWORD bytesToRead, LPDWORD bytesRead);
+
+			bool                             m_hooksSet;
+
+			CacheStatus                      m_stats;
+
+			std::map<HANDLE, CachePointer>   m_cachePointers;
+			std::map<int32_t, CacheObject*>  m_cacheObjects;
+
+			ILogProvider::LogLevel           m_logDebug;
+			ILogProvider*                    m_logger;
+		};
+	}
+}
+
